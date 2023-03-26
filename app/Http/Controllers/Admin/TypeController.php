003f8c3d92684ca56ce00b20953f8b3734c4e52a
Fedois <fedefox14@gmail.com>
2023-03-26 13:12:50 +0200

    aggiunto eliminazione in show type

--- app/Http/Controllers/Admin/TypeController.php
+++ app/Http/Controllers/Admin/TypeController.php
@@ -92,6 +92,8 @@ class TypeController extends Controller
      */
     public function destroy(type $type)
     {
-        //
+        $type->delete();
+
+        return redirect()->route('admin.types.index')->with('success', 'eliminazione avvenuta!');
     }
 }
--- resources/views/admin/types/show.blade.php
+++ resources/views/admin/types/show.blade.php
@@ -17,5 +17,34 @@
                 @endforeach
             @endif
         </ul>
+
+        <form action="{{ route('admin.types.destroy', $type->id) }}" class="d-inline-block" method="POST">
+            @csrf
+            @method('DELETE')
+            
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                elimina
+            </button>
+            
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">conferma eliminazione</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            sei sicuro di voler eliminare??
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">no</button>
+                            <button type="submit" class="btn btn-primary">Salva</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
     </div>
 @endsection
